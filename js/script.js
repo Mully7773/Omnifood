41d1e716394bf415aae49aaa9f0e2c5ca5831b5e
Nick M <mully7773@gmail.com>
2022-08-10 21:44:55 -0500

    Added mobile navigation functionality to the header

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 console.log("Hello world!");
 
+// Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear().toString();
 yearEl.textContent = currentYear;
+
+// Make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
